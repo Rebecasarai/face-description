9daec1eb5d1fa740264a7f730ce188fe71c81464
Rebecasarai <becahere@gmail.com>
2022-09-04 17:00:26 +0200

    Change in model

--- script.js
+++ script.js
@@ -175,7 +175,7 @@ var modelLoaded = false;
 (async () => {
     this.tts=createTts()
     speak(this.tts, "Cargando modelo");
-    model = await tf.loadLayersModel('./model/latest_31_08_2022_small_2_quantize_uint16/model.json')//.then(() => {
+    model = await tf.loadLayersModel('./model/latest_31_08_2022_small_3_quantize_uint16/model.json')//.then(() => {
         
     console.log(tf.getBackend());
     //tf.setBackend('cpu');
